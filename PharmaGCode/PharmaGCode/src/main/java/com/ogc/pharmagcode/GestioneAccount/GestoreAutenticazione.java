a5bfa1b211214e070e87ff4ba97ec3c4a7e2a3cf
vinz321 <vincenzo.micciche.27@gmail.com>
2022-05-31 19:28:55 +0200

    Fixed Graffa che manca

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneAccount/GestoreAutenticazione.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneAccount/GestoreAutenticazione.java
@@ -13,21 +13,24 @@ public class GestoreAutenticazione {
     public GestoreAutenticazione(Stage stage){
         Utils.cambiaInterfaccia("GestioneAccount/Login.fxml",stage, c->{return new ModuloLogin(this);});
     }
-    public void controlloCredenziali(String email, String password, Stage s){
-        if(Main.sistema==0){
-            Utils.cambiaInterfaccia("GestioneFarmaci/InterfacciaFarmacista.fxml",s
-                    ,c->{return new InterfacciaPrincipaleFarmacista("Francesco","Giorgio");
+    public void controlloCredenziali(String email, String password, Stage s) {
+        if (Main.sistema == 0) {
+            Utils.cambiaInterfaccia("GestioneFarmaci/InterfacciaFarmacista.fxml", s
+                    , c -> {
+                        return new InterfacciaPrincipaleFarmacista("Francesco", "Giorgio");
                     });
-        }else if(Main.sistema==1){
-            Utils.cambiaInterfaccia("GestioneConsegna/InterfacciaCorriere.fxml",s
-                    ,c->{return new InterfacciaPrincipaleCorriere("Francesco","Giorgio", "22");
+        } else if (Main.sistema == 1) {
+            Utils.cambiaInterfaccia("GestioneConsegna/InterfacciaCorriere.fxml", s
+                    , c -> {
+                        return new InterfacciaPrincipaleCorriere("Francesco", "Giorgio", "22");
                     });
-        }else if(Main.sistema == 2){
-            Utils.cambiaInterfaccia("GestioneProduzione/InterfacciaImpiegato.fxml",s
-                    ,c->{return new InterfacciaPrincipaleCorriere("Biagio","Conte", "11");
+        } else if (Main.sistema == 2) {
+            Utils.cambiaInterfaccia("GestioneProduzione/InterfacciaImpiegato.fxml", s
+                    , c -> {
+                        return new InterfacciaPrincipaleCorriere("Biagio", "Conte", "11");
                     });
         }
-
+    }
     public static Utente getUtente(){
         return utente;
     }
