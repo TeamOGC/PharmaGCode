ea2bb8736ec76a1681c94ed77dbb7fc56346d211
justlooller <simonegiambrone2001.2@gmail.com>
2022-05-31 19:00:42 +0200

    Completed DB Farmacie 2/2

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/Utils/DBMSDaemon.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/Utils/DBMSDaemon.java
@@ -3,7 +3,7 @@ package com.ogc.pharmagcode.Utils;
 import com.ogc.pharmagcode.Entity.Farmacista;
 import com.ogc.pharmagcode.Entity.Farmaco;
 import com.ogc.pharmagcode.Entity.Utente;
-import com.ogc.pharmagcode.InterfacciaPrincipale;
+import com.ogc.pharmagcode.Main;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -21,10 +21,11 @@ public class DBMSDaemon {
     private static Connection connFarmacia = null;
     private static Connection connAzienda = null;
 
-    public DBMSDaemon() {
-        this.connect();
+    
+    private static void erroreComunicazioneDBMS(Exception e) {
+        Main.log.error("Errore durante comunicazione con DBMS", e);
+        Utils.creaPannelloErrore("C'è stato un problema durante la comunicazione con la base di dati, riprova");
     }
-
 
     private static String buildConnectionUrl(String dbName) {
         if (dbName.equals(DBFarmacie) || dbName.equals(DBAzienda))
@@ -37,21 +38,28 @@ public class DBMSDaemon {
         connectAzienda();
     }
 
-    public void connectFarmacia() {
+    public static void connectFarmacia() {
         try {
-            if (connFarmacia == null || connFarmacia.isClosed())
+            if (connFarmacia == null || connFarmacia.isClosed()) {
+                Main.log.debug("Connettendo con Farmacia...");
                 DBMSDaemon.connFarmacia = DriverManager.getConnection(buildConnectionUrl(DBFarmacie));
+                Main.log.info("Connesso con Farmacia");
+            }
         } catch (java.sql.SQLException e) {
-            e.printStackTrace(System.err);
+            erroreComunicazioneDBMS(e);
+            
         }
     }
 
     public static void connectAzienda() {
         try {
-            if (connAzienda == null || connAzienda.isClosed())
+            if (connAzienda == null || connAzienda.isClosed()) {
+                Main.log.debug("Connettendo con Azienda...");
                 DBMSDaemon.connAzienda = DriverManager.getConnection(buildConnectionUrl(DBAzienda));
+                Main.log.info("Connesso con Azienda");
+            }
         } catch (java.sql.SQLException e) {
-            e.printStackTrace(System.err);
+            erroreComunicazioneDBMS(e);
         }
     }
 
@@ -62,7 +70,7 @@ public class DBMSDaemon {
      * Controlla Credenziali utenza farmacista
      *
      * @param mail Mail dell'utente
-     * @param password Password dell'utente (in chiaro)
+     * @param password Password dell'utente già hashata
      * @return {@link Utente}
      */
     public static Utente F_ControllaCredenziali(String mail, String password) {
@@ -73,14 +81,7 @@ public class DBMSDaemon {
             stmt.setString(2, password);
             var r = stmt.executeQuery();
             if (r.next()) {
-                return new Utente(
-                        r.getInt(1),
-                        r.getInt(2),
-                        r.getString(3),
-                        r.getString(4),
-                        r.getString(5),
-                        null
-                );
+                return Farmacista.createFromDB(r);
             }
         } catch (SQLException e) {
             erroreComunicazioneDBMS(e);
@@ -88,7 +89,6 @@ public class DBMSDaemon {
         return null;
 
     }
-
 
     /**
      *  Verifica l'esistenza di una mail all'interno del DBMS
@@ -140,14 +140,16 @@ public class DBMSDaemon {
      * @param id_farmacia farmacia che ha scaricato il farmaco
      * @return quantità ancora disponibili dalla farmacia
      */
-    public int controlloScorte(int id_lotto, int id_farmacia) {
+    public static int controlloScorte(int id_lotto, int id_farmacia) {
         connectFarmacia();
         var query = "SELECT Lotto.quantita FROM DB_Farmacie.Lotto WHERE id_lotto = ? AND id_farmacia= ?";
         try (PreparedStatement stmt = connFarmacia.prepareStatement(query)) {
             stmt.setInt(1, id_lotto);
             stmt.setInt(2, id_farmacia);
             var r = stmt.executeQuery();
-            return r.getInt(1);
+            if(r.next()) {
+                return r.getInt(1);
+            }
         } catch (SQLException e) {
             erroreComunicazioneDBMS(e);
         }
@@ -162,14 +164,15 @@ public class DBMSDaemon {
      * @param data_caricamento data del caricamento della merce
      * @param qty quantita
      */
-    public void caricaFarmaco(int id_lotto, int id_farmacia, Date data_caricamento, int qty) {
+    public static void caricaFarmaco(int id_lotto, int id_farmacia, Date data_caricamento, int qty) {
         connectFarmacia();
         try (CallableStatement call = connFarmacia.prepareCall("{CALL caricoMerci(?, ?, ?, ?, ?)}")) {
             call.setInt(1, id_lotto);
             call.setInt(2, id_farmacia);
             call.setDate(3, data_caricamento);
             call.setInt(4, qty);
-            call.executeUpdate();
+            call.execute();
+
         } catch (SQLException e) {
             erroreComunicazioneDBMS(e);
         }
@@ -181,16 +184,27 @@ public class DBMSDaemon {
      * @param id_lotto id del lotto che viene scaricato
      * @param id_farmacia id della farmacia che effettua lo scarico
      * @param qty quantità di farmaco che viene scaricato
-     * @return quantità di farmaco ancora disponibile in quella farmacia
+     * @return quantità di farmaco ancora disponibile in quella farmacia, -1 se ha errorato
      */
-    public int scaricaMerci(int id_lotto, int id_farmacia, int qty) {
+    public static int scaricaMerci(int id_lotto, int id_farmacia, int qty) {
         connectFarmacia();
-        String query = "UPDATE DB_Farmacie.Lotto SET Lotto.quantita= ? WHERE Lotto.id_lotto=? AND Lotto.id_farmacia=?";
+        String query = "UPDATE DB_Farmacie.Lotto SET Lotto.quantita=((SELECT Lotto.quantita FROM DB_Farmacie.Lotto WHERE Lotto.id_lotto=?)-?) WHERE Lotto.id_lotto=? AND Lotto.id_farmacia=?";
         try (PreparedStatement stmt= connFarmacia.prepareStatement(query)) {
-            stmt.setInt(1, qty);
-            stmt.setInt(2, id_lotto);
-            stmt.setInt(3, id_farmacia);
-            stmt.executeQuery();
+            stmt.setInt(1, id_lotto);
+            stmt.setInt(2, qty);
+            stmt.setInt(3, id_lotto);
+            stmt.setInt(4, id_farmacia);
+            var r = stmt.executeUpdate();
+            var squery = "SELECT Lotto.quantita FROM DB_Farmacie.Lotto WHERE Lotto.id_lotto=?";
+            try (PreparedStatement sstmt= connFarmacia.prepareStatement(squery)) {
+                sstmt.setInt(1, id_lotto);
+                var risultato = sstmt.executeQuery();
+                if (risultato.next()) {
+                    return risultato.getInt(1);
+                }
+            } catch (SQLException e) {
+                erroreComunicazioneDBMS(e);
+            }
         } catch (SQLException e) {
             erroreComunicazioneDBMS(e);
         }
@@ -202,9 +216,9 @@ public class DBMSDaemon {
      *
      * @param id_farmacia id della farmacia da cui parte la richiesta
      * @param data_caricamento data in cui parte la richiesta
-     * @return "risultato" è un resultset che contiene nella colonna 1 l'id lotto e nella colonna 2 la quantità caricata
+     * @return "risultato" è un {@link ResultSet} che contiene nella colonna 1 l'id lotto e nella colonna 2 la quantità caricata
      */
-    public ResultSet queryMerceCaricata(int id_farmacia, Date data_caricamento) {
+    public static ResultSet queryMerceCaricata(int id_farmacia, Date data_caricamento) {
         connectFarmacia();
         String query = "SELECT Caricamenti.id_lotto, Caricamenti.quantita FROM Caricamenti WHERE Caricamenti.data_caricamento=? AND Caricamenti.id_farmacia=?";
         try (PreparedStatement stmt = connFarmacia.prepareStatement(query)) {
@@ -246,7 +260,7 @@ public class DBMSDaemon {
     }
 
 
-    public void testQuery() {
+    public static void testQuery() {
         connectFarmacia(); // provo a riconnettermi ad ogni query, metti caso che parte la connessione per qualche motivo
         var query = "SELECT Farmaco.* FROM DB_Farmacie.Farmaco WHERE Farmaco.nome = ?";
         try (PreparedStatement stmt = connFarmacia.prepareStatement(query)) {
