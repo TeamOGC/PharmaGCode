5c7bede7376baca170434eb6e4f0ec7039894fe6
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-06-05 18:50:06 +0200

    Converted ListaOrdiniPeriodici to TableView

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreListaOrdiniPeriodici.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreListaOrdiniPeriodici.java
@@ -1,12 +1,43 @@
 package com.ogc.pharmagcode.GestioneFarmaci;
 
+import com.ogc.pharmagcode.Entity.OrdinePeriodico;
+import com.ogc.pharmagcode.Main;
+import com.ogc.pharmagcode.Utils.DBMSDaemon;
+import com.ogc.pharmagcode.Utils.TableEntities.RecordOrdinePeriodico;
 import com.ogc.pharmagcode.Utils.Utils;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class GestoreListaOrdiniPeriodici {
     private InterfacciaListaOrdiniPeriodici i;
-    public GestoreListaOrdiniPeriodici(){
-        i=(InterfacciaListaOrdiniPeriodici) Utils.cambiaInterfaccia("GestioneFarmaci/ListaOrdiniPeriodici.fxml",new Stage(), c->{
+
+    public ObservableList<RecordOrdinePeriodico> recordOrdinePeriodicoObservableList;
+
+    public GestoreListaOrdiniPeriodici() {
+
+        ArrayList<RecordOrdinePeriodico> ordiniPeriodici = new ArrayList<>();
+        OrdinePeriodico[] ordini = DBMSDaemon.queryOrdiniPeriodici();
+
+        if (ordini != null) {
+            for (OrdinePeriodico ordine : ordini) {
+                Main.log.info(ordine);
+                ordiniPeriodici.add(
+                        RecordOrdinePeriodico.fromOrdinePeriodico(
+                                ordine,
+                                "Modifica",
+                                modifica -> {
+                                    new GestoreModificaOrdinePeriodico(ordine);
+                                }
+                        )
+                );
+            }
+        }
+        recordOrdinePeriodicoObservableList = FXCollections.observableArrayList(ordiniPeriodici);
+
+        i = (InterfacciaListaOrdiniPeriodici) Utils.cambiaInterfaccia("GestioneFarmaci/ListaOrdiniPeriodici.fxml", new Stage(), c -> {
             return new InterfacciaListaOrdiniPeriodici(this);
         });
     }
--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreModificaOrdinePeriodico.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreModificaOrdinePeriodico.java
@@ -1,14 +1,29 @@
 package com.ogc.pharmagcode.GestioneFarmaci;
 
+import com.ogc.pharmagcode.Entity.OrdinePeriodico;
+import com.ogc.pharmagcode.Main;
+import com.ogc.pharmagcode.Utils.DBMSDaemon;
+import com.ogc.pharmagcode.Utils.TableEntities.RecordOrdinePeriodico;
 import com.ogc.pharmagcode.Utils.Utils;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
 public class GestoreModificaOrdinePeriodico {
     private InterfacciaModificaOrdinePeriodico i;
 
-    public GestoreModificaOrdinePeriodico() {
+    private OrdinePeriodico ordinePeriodico;
+
+    public GestoreModificaOrdinePeriodico(OrdinePeriodico ordinePeriodico) {
         i = (InterfacciaModificaOrdinePeriodico) Utils.cambiaInterfaccia("GestioneFarmaci/ModificaOrdinePeriodico.fxml", new Stage(), c -> {
-            return new InterfacciaModificaOrdinePeriodico(this);
+            return new InterfacciaModificaOrdinePeriodico(this, ordinePeriodico);
         }, 600, 400);
+        this.ordinePeriodico = ordinePeriodico;
+    }
+
+    public void aggiornaQuantita(int qta, OrdinePeriodico ordinePeriodico){
+        Main.log.info("Aggiornando la quantità dell'ordine periodico (" + qta + ") di " + ordinePeriodico.getNomeFarmaco() + ";" );
+        DBMSDaemon.queryAggiornaOrdinePeriodico(qta, ordinePeriodico);
     }
 }
