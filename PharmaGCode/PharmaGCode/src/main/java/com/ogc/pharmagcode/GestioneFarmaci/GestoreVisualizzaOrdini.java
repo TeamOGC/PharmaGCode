75828d73bdd72f49c74e00607ebf788962a1696e
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-06-05 15:36:16 +0200

    Converted VisualizzaOrdiniFarmacia into TableView

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreVisualizzaOrdini.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneFarmaci/GestoreVisualizzaOrdini.java
@@ -4,6 +4,7 @@ import com.ogc.pharmagcode.Entity.Ordine;
 import com.ogc.pharmagcode.Main;
 import com.ogc.pharmagcode.Utils.DBMSDaemon;
 import com.ogc.pharmagcode.Utils.RecordLista;
+import com.ogc.pharmagcode.Utils.RecordOrdine;
 import com.ogc.pharmagcode.Utils.Utils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 
 public class GestoreVisualizzaOrdini {
     private InterfacciaVisualizzaOrdini i;
-    private ArrayList<RecordLista> listaOrdini=new ArrayList<>();
+    private final ArrayList<RecordOrdine> listaOrdini=new ArrayList<>();
     private LocalDate d;
     public GestoreVisualizzaOrdini(){
         d=Main.orologio.chiediOrario().toLocalDate();
@@ -25,24 +26,21 @@ public class GestoreVisualizzaOrdini {
                 c->{return new InterfacciaVisualizzaOrdini(this);});
     }
 
-    public ArrayList<RecordLista> chiediOrdini(){
+    public ArrayList<RecordOrdine> chiediOrdini(){
         Ordine[] ordini=DBMSDaemon.queryVisualizzaOrdiniFarmacia(Main.idFarmacia);
-        for(Ordine o:ordini){
-            EventHandler<ActionEvent> e=n->{};
-            String bottone="";
-            if(Duration.between(d.atTime(0,0,1), o.getData_consegna().atTime(0,0,1)).toDays()>1){
-                e=modifica->{new GestoreModificaOrdine(o);};
-                bottone="Modifica";
-            }else if(d.equals(o.getData_consegna())){
-                e=carica->{new GestoreCaricoMerci(o.getId_ordine());};
-                bottone="Carica";
+        if(ordini != null){
+            for(Ordine o:ordini){
+                EventHandler<ActionEvent> e=n->{};
+                String bottone="";
+                if(Duration.between(d.atTime(0,0,1), o.getData_consegna().atTime(0,0,1)).toDays()>1){
+                    e=modifica->{new GestoreModificaOrdine(o);};
+                    bottone="Modifica";
+                }else if(d.equals(o.getData_consegna())){
+                    e=carica->{new GestoreCaricoMerci(o.getId_ordine());};
+                    bottone="Carica";
+                }
+                listaOrdini.add(RecordOrdine.fromOrdine(o, bottone, e));
             }
-            listaOrdini.add(new RecordLista(e,984,
-                            o.getNome_farmaco()+"",
-                            o.getStato(),o.getQuantita()+"",
-                            o.getData_consegna().format(DateTimeFormatter.ISO_LOCAL_DATE),
-                            bottone));
-
         }
         return listaOrdini;
     }
