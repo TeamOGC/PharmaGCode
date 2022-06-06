29b3dd7bb6c38f4dad9fa1961a96679921441934
justlooller <simonegiambrone2001.2@gmail.com>
2022-06-06 16:44:04 +0200

    Updated aggiornaquantitaOrdine

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/Utils/DBMSDaemon.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/Utils/DBMSDaemon.java
@@ -745,25 +745,37 @@ public class DBMSDaemon {
     }
 
     /**
-     * aggiorna la quantità ordinabile (?)
+     * aggiorna la quantità di un ordine, se la quantità è settata a 0 elimina l'ordine
      *
-     * @param ordine {@link Ordine} ordine di cui modificare la quantita
+     * @param ordine {@link Ordine} ordine di cui modificare la quantità
      * @return 1 if success, -1 if error
      */
-    public static int queryAggiornaQuantitaOrdine(Ordine ordine){
+    public static int queryAggiornaQuantitaOrdine(Ordine ordine) {
         connectAzienda();
-        String query= "UPDATE Ordine SET Ordine.quantita=? WHERE Ordine.id_ordine=?";
-        try(PreparedStatement stmt=connAzienda.prepareStatement(query)){
-            stmt.setInt(1, ordine.getQuantita());
-            stmt.setInt(2,ordine.getId_ordine());
-            var r=stmt.executeUpdate();
-            if (r !=0)
-                return r;
+        if (ordine.getQuantita() == 0) {
+            String query = "DELETE FROM Ordine WHERE id_ordine=?";
+            try (PreparedStatement stmt = connAzienda.prepareStatement(query)) {
+                stmt.setInt(1, ordine.getId_ordine());
+                var r = stmt.executeUpdate();
+                if (r != 0)
+                    return r;
+            } catch (SQLException e) {
+                erroreComunicazioneDBMS(e);
+            }
+            return -1;
+        } else {
+            String query = "UPDATE Ordine SET Ordine.quantita=? WHERE Ordine.id_ordine=?";
+            try (PreparedStatement stmt = connAzienda.prepareStatement(query)) {
+                stmt.setInt(1, ordine.getQuantita());
+                stmt.setInt(2, ordine.getId_ordine());
+                var r = stmt.executeUpdate();
+                if (r != 0)
+                    return r;
+            } catch (SQLException e) {
+                erroreComunicazioneDBMS(e);
+            }
+            return -1;
         }
-        catch (SQLException e){
-            erroreComunicazioneDBMS(e);
-        }
-        return -1;
     }
 
     /**
