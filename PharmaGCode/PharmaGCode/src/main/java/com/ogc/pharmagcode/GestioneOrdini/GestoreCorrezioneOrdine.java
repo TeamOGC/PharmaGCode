08218049007f78687a012fd90289e44e744b65b7
ExalFabu <53974096+ExalFabu@users.noreply.github.com>
2022-06-08 19:12:43 +0200

    Feature: Correggi Ordine lato azienda
    + Reformat DBMS Daemon

--- PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneOrdini/GestoreCorrezioneOrdine.java
+++ PharmaGCode/PharmaGCode/src/main/java/com/ogc/pharmagcode/GestioneOrdini/GestoreCorrezioneOrdine.java
@@ -2,6 +2,7 @@ package com.ogc.pharmagcode.GestioneOrdini;
 
 import com.ogc.pharmagcode.Entity.Ordine;
 import com.ogc.pharmagcode.Main;
+import com.ogc.pharmagcode.Utils.DBMSDaemon;
 import com.ogc.pharmagcode.Utils.Utils;
 import javafx.stage.Stage;
 
@@ -10,12 +11,35 @@ public class GestoreCorrezioneOrdine {
     private final InterfacciaCorrezioneOrdine boundary;
     private Ordine daCorreggere;
 
-    public GestoreCorrezioneOrdine(Ordine daCorreggere){
+    public GestoreCorrezioneOrdine(Ordine daCorreggere) {
         Main.log.info("Tentando di correggere  l'ordine " + daCorreggere.getData_consegna());
-        this.boundary=(InterfacciaCorrezioneOrdine) Utils.cambiaInterfaccia("GestioneOrdini/CorrezioneOrdine.fxml",
+        this.boundary = (InterfacciaCorrezioneOrdine) Utils.cambiaInterfaccia("GestioneOrdini/CorrezioneOrdine.fxml",
                 new Stage(),
-                c->{return new InterfacciaCorrezioneOrdine(daCorreggere);});
+                c -> new InterfacciaCorrezioneOrdine(this, daCorreggere),
+                600,
+                400
+                );
         this.daCorreggere = daCorreggere;
+    }
+
+    public void correggiOrdine(int qtaDaAggiungere) {
+        int qtaGiaCaricata = DBMSDaemon.queryControllaQuantita(daCorreggere);
+        if(qtaGiaCaricata == -1 ) {
+            Main.log.error("Per qualche motivo la quantità caricata non è stata presa...");
+            return;
+        }
+        Main.log.info("Quantità già caricata: " + qtaGiaCaricata);
+        if(qtaDaAggiungere > (daCorreggere.getQuantita() - qtaGiaCaricata)){
+            // ho voluto aggiungere più farmaci di quanti ne mancano
+            Utils.creaPannelloErrore("Non puoi correggere l'ordine inserendo più farmaci di quanti ne mancano");
+            return;
+        }
+
+//        DBMSDaemon.queryCorreggiOrdine(qtaDaAggiungere, qtaGiaCaricata, daCorreggere);
+        daCorreggere.setStato("Corretto");
+
+        Main.log.info("Ordine corretto");
+        ((Stage) boundary.qtaIntegrare.getScene().getWindow()).close();
     }
 }
 
